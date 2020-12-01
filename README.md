c3f1f6446d98d59a95d48c854fc995ae02a28b7f
VyrekXD <vyrekxd@gmail.com>
2020-12-01 16:01:22 -0600

    The first version uwu

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quick Themes Toggle
+A [powercord](https://powercord.dev/) plugin that using F6 and F7 toggle **all** the themes
+F6: Turn off al the themes
+F7: Turn on all the themes
+
+# How to install
+Open your terminal and go to your themes folder *C:\Users\<user>\powercord\src\Powercord\themes*
+And then put git pull https://github.com/VyrekXD/quick-themes-toggle
---
+++ index.js
@@ -0,0 +1,34 @@
+//i grab a part of the code from https://github.com/ezist/quick-toggler/ by ezist#7571
+
+const { Plugin } = require('powercord/entities')
+
+module.exports = class QuickThemeToggler extends Plugin {
+    async startPlugin () {
+        this._themesStatus = true
+        document.addEventListener('keydown', this.onKeyDownWrapper.bind(this))
+    }
+
+    pluginWillUnload () { document.removeEventListener(this.onKeyDownWrapper.bind(this)) }
+
+    onKeyDown (e) {
+      switch (e.code) {
+        case 'F6':
+            if(!this._themesStatus)return;
+
+            powercord.styleManager.unloadThemes()
+
+            this._themesStatus = false;
+          break
+        case 'F7': {
+            if(this._themesStatus === true)return;
+
+            powercord.styleManager.loadThemes()
+
+            this._themesStatus = true;
+          break
+        }
+      }
+    }
+  
+    onKeyDownWrapper (e) { this.onKeyDown(e) }
+}
